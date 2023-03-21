e96ace15a9cacce0839851659d9f3d2eec41efb7
themagicianking <tmoonder@gmail.com>
2023-03-21 18:42:33 -0400

    Finished writing the function to play one round.

--- javascript.js
+++ javascript.js
@@ -15,3 +15,31 @@ function getComputerChoice(x) {
 
 getComputerChoice(x);
 console.log(computerChoice);
+
+let outcome;
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === "Rock" && computerChoice === "Paper") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        return "You Lose! Scissors beat Paper";
+    }
+    else if (playerChoice === "Scissor" && computerChoice === "Rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+    else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        return "You Win! Paper beats Rock";
+    }
+    else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        return "You Win! Scissors beats Paper";
+    }
+    else {
+        return "It's a tie!"
+    }
+}
+
+playRound("Rock", "Scissors");
